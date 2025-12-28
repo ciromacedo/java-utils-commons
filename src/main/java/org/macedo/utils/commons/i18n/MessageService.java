71f3c2d5ef119141f88839d854fbe454c8c2f869
ciromacedo <ciromacedo@gmail.com>
2025-12-28 06:38:52 -0300

    new features

---
+++ src/main/java/org/macedo/utils/commons/i18n/MessageService.java
@@ -0,0 +1,21 @@
+package org.macedo.utils.commons.i18n;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageService {
+
+    private final MessageSource messageSource;
+
+    public MessageService(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public String getMessage(String key, Object... args) {
+        return messageSource.getMessage(key, args, LocaleContextHolder.getLocale());
+    }
+
+}
+
---
+++ src/main/java/org/macedo/utils/commons/text/Texto.java
@@ -0,0 +1,11 @@
+package org.macedo.utils.commons.text;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Texto {
+
+    public static Boolean isNotBlank(String texto) {
+        return StringUtils.isNotBlank(texto);
+    }
+
+}
