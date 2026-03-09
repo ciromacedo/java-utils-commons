3a17337e0d1b43850c619d65b6e8bc72051cef7a
ciromacedo <ciromacedo@gmail.com>
2026-03-09 15:24:43 -0300

    new

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java-utils-commons
 Classes utilitárias
+
+
+deploy de nova versão:
+
+mvn clean deploy
--- src/main/java/org/macedo/utils/commons/text/Texto.java
+++ src/main/java/org/macedo/utils/commons/text/Texto.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Texto {
 
+    public static final String CRLF = "\r\n";
+    public static final String TAB = "\t";
+    public static final String EMPTY = "";
+    public static final String EMPTY_SPACE = " ";
+
     public static Boolean isNotBlank(String texto) {
         return StringUtils.isNotBlank(texto);
     }
